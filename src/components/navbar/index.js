2032e6e9e649dd466fca5f052afc089229efb457
Faizan Ali <ibrahim.nabi@sprintx.net>
2022-10-17 19:32:19 +0500

    social authentication

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,18 +1,21 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Avatar from "../avatar/index";
-
+import { FaUser } from "react-icons/fa";
 import logoutIcon from "../../assets/icons/logoutIcon.svg";
 import style from "./navbar.module.scss";
 
-function Navbar() {
+function Navbar({ user }) {
   const navigate = useNavigate();
+  console.log(user, "nav user");
   return (
     <div className={style.navDiv}>
       <div className={style.userInfoDiv}>
-        <Avatar initials={"faizan ali"} />
-        <h2>Faizan ali</h2>
+        <div className={style.userLogo}>
+          <FaUser />
+        </div>
+        <h2>{user?.userName}</h2>
+        <h3>{`(${user?.userType})`}</h3>
       </div>
       <h1>Contact List Web App</h1>
       <img
